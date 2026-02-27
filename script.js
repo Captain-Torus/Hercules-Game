dd43108714939782a67d6863c51a8711f09723b4
Captain-Torus <byron.baxley@gmail.com>
2026-02-27 06:59:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-const introText = "Welcome champion, you have selected to complete twelve tasks. But be warned: these are tasks no mortal man could complete.";
-const textElement = document.getElementById("intro-text");
-const startBtn = document.getElementById("start-btn");
 
-let index = 0;
-
-function typeWriter() {
-    if (index < introText.length) {
-        textElement.innerHTML += introText.charAt(index);
-        index++;
-        setTimeout(typeWriter, 40); 
-    } else {
-        startBtn.style.display = "inline-block";
-    }
-}
-
-// Start immediately
-console.log("Game script loaded!");
-typeWriter();
